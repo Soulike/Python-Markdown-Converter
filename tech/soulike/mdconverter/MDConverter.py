a99cce2ac30bfface22fa4fb95d96df6a0b00968
Soulike <316413310@qq.com>
2018-11-02 21:40:29 +0800

    基本完成

--- tech/soulike/mdconverter/MDConverter.py
+++ tech/soulike/mdconverter/MDConverter.py
@@ -1,3 +1,4 @@
+import cgi
 from tech.soulike.mdconverter.regex import *
 
 
@@ -5,52 +6,80 @@ class MDConverter:
 
     def makeHtml(self, code):
 
-        codeParts = self.__splitToPlainAndCode(code)
+        plainAndCodeParts = MDConverter.__splitToPlainAndCode(code)
         processedCode = ''
 
-        for codePart in codeParts:
-            if self.__isCodeBlock(codePart):
-                processedCode += self.__convertCodeBlocks(codePart)
+        for plainOrCodePart in plainAndCodeParts:
+            if MDConverter.__isCodeBlock(plainOrCodePart):
+                plainOrCodePart = MDConverter.__convertCodeBlocks(plainOrCodePart)
             else:
                 # 处理行内元素
-                codePart = self.__convertHeadings(codePart)
-                codePart = self.__convertInlines(CODE, codePart, '<code>')
-                codePart = self.__convertInlines(STRONG, codePart, '<strong>')
-                codePart = self.__convertInlines(ITALIC, codePart, '<i>')
-                # 处理链接和图片，注意这两个有顺序要求，必须先图片后链接
-                codePart = self.__convertImages(codePart)
-                codePart = self.__convertLinks(codePart)
+                plainParts = MDConverter.__splitByDoubleSpace(plainOrCodePart)
+                plainOrCodePart = ''
+                for plainPart in plainParts:
+                    plainPart = MDConverter.__convertHorizontalLines(plainPart)
+                    plainPart = MDConverter.__convertHeadings(plainPart)
+                    plainPart = MDConverter.__convertInlineCodes(plainPart)
+                    plainPart = MDConverter.__convertInlines(STRONG, plainPart, '<strong>')
+                    plainPart = MDConverter.__convertInlines(ITALIC, plainPart, '<i>')
+                    # 处理链接和图片，注意这两个有顺序要求，必须先图片后链接
+                    plainPart = MDConverter.__convertImages(plainPart)
+                    plainPart = MDConverter.__convertLinks(plainPart)
 
-                codePart = self.__convertBlockquotes(codePart)
-                codePart = self.__convertLists(codePart)
+                    plainPart = MDConverter.__convertBlockquotes(plainPart)
+                    plainPart = MDConverter.__convertLists(plainPart)
 
-                # 转换自然段的必须在最后
-                codePart = self.__convertParagraphs(codePart)
+                    # 转换自然段的必须在最后
+                    plainPart = MDConverter.__convertParagraphs(plainPart)
 
-                processedCode += codePart
+                    plainOrCodePart += plainPart
+
+            processedCode += plainOrCodePart
+            print(plainOrCodePart)
 
         return processedCode
 
     # 转换函数部分
 
-    def __convertHeadings(self, code):
+    @staticmethod
+    def __convertHeadings(code):
         results = HEADING.search(code)
         while results is not None:
             level = len(results.group(1))
             convertedStr = '<h{0}>{1}</h{2}>'.format(level, results.group(2), level)
-            code = re.sub(HEADING, convertedStr, code, 1)
+            code = MDConverter.__stringReplace(code, results.start(), results.end(), convertedStr)
             results = HEADING.search(code)
         return code
 
-    def __convertInlines(self, regex, code, startTag):
+    @staticmethod
+    def __convertInlineCodes(code):
+        results = CODE.search(code)
+        while results is not None:
+            convertedStr = '<code>{0}</code>'.format(cgi.escape(results.group(1)))
+            code = MDConverter.__stringReplace(code, results.start(), results.end(), convertedStr)
+            results = CODE.search(code)
+        return code
+
+    @staticmethod
+    def __convertInlines(regex, code, startTag):
         results = regex.search(code)
         while results is not None:
             convertedStr = '{0}{1}{2}'.format(startTag, results.group(1), startTag[:1] + '/' + startTag[1:])
-            code = re.sub(regex, convertedStr, code, 1)
+            code = MDConverter.__stringReplace(code, results.start(), results.end(), convertedStr)
             results = regex.search(code)
         return code
 
-    def __convertParagraphs(self, code):
+    @staticmethod
+    def __convertHorizontalLines(code):
+        results = HORIZONTAL_LINE.search(code)
+        while results is not None:
+            convertedStr = '<hr/>'
+            code = MDConverter.__stringReplace(code, results.start(), results.end(), convertedStr)
+            results = HORIZONTAL_LINE.search(code)
+        return code
+
+    @staticmethod
+    def __convertParagraphs(code):
         results = PLAIN_TEXT.search(code)
         searchGroup = None
         searchedLength = 0
@@ -61,37 +90,39 @@ class MDConverter:
                     break
             if searchGroup is not None and NOT_PLAIN_TEXT.search(searchGroup) is None:
                 convertedStr = '<p>{0}</p>'.format(searchGroup)
-                code = code[:results.start()] + convertedStr + code[results.end():]
+                code = MDConverter.__stringReplace(code, results.start(), results.end(), convertedStr)
             searchedLength += len(searchGroup)
             results = PLAIN_TEXT.search(code, searchedLength)
         return code
 
-    def __convertImages(self, code):
+    @staticmethod
+    def __convertImages(code):
         regex = IMAGE
         results = regex.search(code)
         while results is not None:
             convertedStr = '<img src="{1}" alt="{0}" />'.format(results.group(1), results.group(2))
-            code = re.sub(regex, convertedStr, code, 1)
+            code = MDConverter.__stringReplace(code, results.start(), results.end(), convertedStr)
             results = regex.search(code)
         return code
 
-    def __convertLinks(self, code):
+    @staticmethod
+    def __convertLinks(code):
         regex = LINK
         results = regex.search(code)
         while results is not None:
             convertedStr = '<a href="{1}">{0}</a>'.format(results.group(1), results.group(2))
-            code = re.sub(regex, convertedStr, code, 1)
+            code = MDConverter.__stringReplace(code, results.start(), results.end(), convertedStr)
             results = regex.search(code)
         return code
 
-    def __convertBlockquotes(self, code):
+    @staticmethod
+    def __convertBlockquotes(code):
         regex = BLOCKQUOTE
-        blockcodePartSearchResult = regex.search(code)
+        blockquotePartSearchResult = regex.search(code)
 
         # 找到所有块引用，一块一块的处理
-        while blockcodePartSearchResult is not None:
-            partCode = blockcodePartSearchResult.group(1)  # 某一块的代码
-            originalPartCode = blockcodePartSearchResult.group(1)
+        while blockquotePartSearchResult is not None:
+            partCode = blockquotePartSearchResult.group(1)  # 某一块的代码
             partRegex = BLOCKQUOTE_ITEM  # 代码内部替换用正则
 
             blockquoteSearchResultInPart = partRegex.search(partCode)  # 找到一个引用行
@@ -100,7 +131,7 @@ class MDConverter:
             convertedStr = '<blockquote><p>'
 
             while blockquoteSearchResultInPart is not None:
-                currentBlockQuoteLevel = len(blockquoteSearchResultInPart.group(2))
+                currentBlockQuoteLevel = len(blockquoteSearchResultInPart.group(1))
                 if currentBlockQuoteLevel > maxBlockquoteLevel:
                     maxBlockquoteLevel = currentBlockQuoteLevel
                 blockquoteLevelDiff = currentBlockQuoteLevel - lastBlockquoteLevel  # 看看这一个匹配和上一个匹配是不是一个等级的块引用，如果不是就得特殊处理
@@ -111,11 +142,13 @@ class MDConverter:
                 if blockquoteLevelDiff < 0:
                     blockquoteLevelDiff = 0
 
-                blockquoteContent = blockquoteSearchResultInPart.group(3)
+                blockquoteContent = blockquoteSearchResultInPart.group(2)
 
                 convertedStr += '<blockquote>' * blockquoteLevelDiff + '<p>' * (blockquoteLevelDiff % 2) + \
                                 blockquoteContent
-                partCode = re.sub(partRegex, convertedStr, partCode, 1)
+
+                partCode = MDConverter.__stringReplace(partCode, blockquoteSearchResultInPart.start(),
+                                                       blockquoteSearchResultInPart.end(), convertedStr)
                 convertedStr = ''
                 blockquoteSearchResultInPart = partRegex.search(partCode)  # 再次查找在这个代码块里面还有没有块引用
 
@@ -123,18 +156,21 @@ class MDConverter:
             partCode += '</p></blockquote>'
             partCode += '</blockquote>' * maxBlockquoteLevel
 
-            code = re.sub(originalPartCode, partCode, code, 1)
+            code = MDConverter.__stringReplace(code, blockquotePartSearchResult.start(1),
+                                               blockquotePartSearchResult.end(1), partCode)
 
-            nextSearchStartIndex = blockcodePartSearchResult.end()  # 记录一下下次应该从哪里开始匹配
-            blockcodePartSearchResult = regex.search(code, nextSearchStartIndex)
+            nextSearchStartIndex = blockquotePartSearchResult.end()  # 记录一下下次应该从哪里开始匹配
+            blockquotePartSearchResult = regex.search(code, nextSearchStartIndex)
         return code
 
-    def __convertLists(self, code):
+    @staticmethod
+    def __convertLists(code):
         regex = LIST
-
         listPartSearchResult = regex.search(code)
         while listPartSearchResult is not None:
             partCode = listPartSearchResult.group(1)
+            if partCode is None:
+                partCode = listPartSearchResult.group(2)
 
             partRegex = LIST_ITEM
             partSearchResult = partRegex.search(partCode)
@@ -142,7 +178,7 @@ class MDConverter:
 
             closeTagStack = []
 
-            if self.__isOrderedListItem(partCode) is True:
+            if MDConverter.__isOrderedListItem(partCode) is True:
                 convertedStr = '<ol>'
                 closeTagStack.append('</ol>')
                 isOrderedItem = True
@@ -152,18 +188,18 @@ class MDConverter:
                 isOrderedItem = False
 
             while partSearchResult is not None:
-                currentListLevel = len(partSearchResult.group(2)) // 4
+                currentListLevel = len(partSearchResult.group(1)) // 4
                 listLevelDiff = currentListLevel - lastListLevel
                 lastListLevel = currentListLevel
 
-                listContent = partSearchResult.group(3)
+                listContent = partSearchResult.group(2)
 
                 if listLevelDiff < 0:
                     convertedStr += closeTagStack.pop()
                     for i in range(2 * (-listLevelDiff)):
                         convertedStr += closeTagStack.pop()
                 elif listLevelDiff > 0:
-                    if self.__isOrderedListItem(partSearchResult.group(0)) is True:
+                    if MDConverter.__isOrderedListItem(partSearchResult.group(0)) is True:
                         convertedStr += '<ol>'
                         closeTagStack.append('</ol>')
                         isOrderedItem = True
@@ -174,7 +210,7 @@ class MDConverter:
                 elif len(closeTagStack) is not 1:
                     convertedStr += closeTagStack.pop()
 
-                    if self.__isOrderedListItem(partSearchResult.group(0)) is not isOrderedItem:
+                    if MDConverter.__isOrderedListItem(partSearchResult.group(0)) is not isOrderedItem:
                         isOrderedItem = not isOrderedItem
                         convertedStr += closeTagStack.pop()
                         if isOrderedItem is True:
@@ -186,30 +222,34 @@ class MDConverter:
 
                 convertedStr += '<li>' + listContent
                 closeTagStack.append('</li>')
-                partCode = re.sub(partRegex, convertedStr, partCode, 1)
+                partCode = MDConverter.__stringReplace(partCode, partSearchResult.start(), partSearchResult.end(),
+                                                       convertedStr)
                 convertedStr = ''
                 partSearchResult = partRegex.search(partCode)
             for i in range(len(closeTagStack)):
                 partCode += closeTagStack.pop()
-            code = re.sub(listPartSearchResult.group(1), partCode, code, 1)
+            code = MDConverter.__stringReplace(code, listPartSearchResult.start(1), listPartSearchResult.end(1),
+                                               partCode)
             nextSearchStartIndex = listPartSearchResult.end()
             listPartSearchResult = regex.search(code, nextSearchStartIndex)
         return code
 
-    def __convertCodeBlocks(self, code):
+    @staticmethod
+    def __convertCodeBlocks(code):
         regex = CODE_BLOCK
         results = regex.search(code)
         while results is not None:
             convertedStr = '<pre><code class="{0} language-{0}">{1}</code></pre>'.format(results.group(1),
-                                                                                         results.group(2))
-            code = re.sub(regex, convertedStr, code, 1)
+                                                                                         cgi.escape(results.group(2)))
+            code = MDConverter.__stringReplace(code, results.start(), results.end(), convertedStr)
             results = regex.search(code)
         return code
 
     # 工具函数部分
 
     # 判定一个列表项是否是ol项，返回值是布尔值
-    def __isOrderedListItem(self, listItem):
+    @staticmethod
+    def __isOrderedListItem(listItem):
         typeSearchResult = LIST_ITEM_TYPE.search(listItem)
         if typeSearchResult.group(2) is not None and typeSearchResult.group(2).isdigit():
             return True
@@ -217,7 +257,8 @@ class MDConverter:
             return False
 
     # 将整个代码以代码块为分隔分割为多个部分，返回一个列表，其中按顺序放置各个代码块
-    def __splitToPlainAndCode(self, code):
+    @staticmethod
+    def __splitToPlainAndCode(code):
         codeBlocks = []
         lastCodeBlockEnd = 0  # 上一个代码块的结束位置
         codeBlockSearchResults = CODE_BLOCK.finditer(code)
@@ -231,5 +272,16 @@ class MDConverter:
         return codeBlocks
 
     # 判定一个块是否是代码块
-    def __isCodeBlock(self, code):
+    @staticmethod
+    def __isCodeBlock(code):
         return CODE_BLOCK.search(code) is not None
+
+    # 将一个块用双空白分割
+    @staticmethod
+    def __splitByDoubleSpace(code):
+        return re.split('(?: {2,}$)|(?:(?:\r?\n){2,})', code, 0, flags = re.MULTILINE)
+
+    # 字符串内容替换
+    @staticmethod
+    def __stringReplace(string, startIndex, endIndex, insertedString):
+        return string[:startIndex] + insertedString + string[endIndex:]
--- tech/soulike/mdconverter/regex.py
+++ tech/soulike/mdconverter/regex.py
@@ -1,32 +1,34 @@
 import re
 
 # 行内元素
-HEADING = re.compile('^(#+) (.*)(?:\r?\n)+', re.MULTILINE)
-CODE = re.compile('`([^`\r\n]+)(?:\r?\n)*`')
-STRONG = re.compile('(?:\r?\n)*\*{2}(.+)\*{2}(?:\r?\n)*')
-ITALIC = re.compile('(?:\r?\n)*\*(.+)\*(?:\r?\n)*')
+HEADING = re.compile('^(#{1,6}) (.*)', re.MULTILINE)
+CODE = re.compile('`([^`\r\n]+)`')
+STRONG = re.compile('(?:\r?\n)*[*]{2}(.+)[*]{2}')
+ITALIC = re.compile('(?:\r?\n)*[*](.+)[*]')
+HORIZONTAL_LINE = re.compile('^-{3,}', re.MULTILINE)
 
 # 图片与链接
-IMAGE = re.compile('(?:\r?\n)*!\[(.+)\]\((.+)\)(?:\r?\n)*')
-LINK = re.compile('(?:\r?\n)*\[(.+)\]\((.+)\)(?:\r?\n)*')
+IMAGE = re.compile('!\[(.+?)\]\((.+?)\)')
+LINK = re.compile('\[(.+?)\]\((.+?)\)')
 
 # 引用
 # 用于判定引用块
-BLOCKQUOTE = re.compile('(?:<.+?>.*</.+?>|<.+?/>)*\s*(> .+?)(?:\s{2,}|<\w+>|$)', re.DOTALL)
+BLOCKQUOTE = re.compile('^(> .+)+', re.DOTALL | re.MULTILINE)
 # 用于在引用块内匹配内容
-BLOCKQUOTE_ITEM = re.compile('(<\w+>)*>(>*) (.+)')
+BLOCKQUOTE_ITEM = re.compile('^>(>*) (.+?)$', re.MULTILINE)
 
 # 列表
 # 用于判定列表块
-LIST = re.compile('(?:<.+?>.*</.+?>|<.+?/>)*((?:(?: {4})*(?:([\-*+]|\d\.) .+?\r?\n?))+)(?:\s{2,}|<\w+>|$)', re.DOTALL)
+LIST = re.compile('(^(?: {4})*(?:[\-*+]|\d\.) .+)+', re.DOTALL | re.MULTILINE)
 # 用于在列表内匹配内容
-LIST_ITEM = re.compile('(<\w+>)*( *)(?:[*\-+]|\d\.) (.+)')
+LIST_ITEM = re.compile('^((?: {4})*)(?:[*\-+]|\d\.) (.+?)$', re.MULTILINE)
 # 用于判定列表项类型
 LIST_ITEM_TYPE = re.compile('\s*((\d+)\.|[\-*+]) .*')
 
+
 # 代码块
 # 代码块判定
-CODE_BLOCK = re.compile('^```(\w+)?\r?\n(.+)```$', re.DOTALL | re.MULTILINE)
+CODE_BLOCK = re.compile('^```(\w+)?\r?\n(.+?)```$', re.DOTALL | re.MULTILINE)
 
 # 普通文本
 PLAIN_TEXT = re.compile('(?:^(\S*\s?\S+)\s{2,})|(?:\s{2,}(\S*\s?\S+)$)', re.MULTILINE)
